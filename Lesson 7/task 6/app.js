a5021a781e89b1e8ee5de6d407a0b8f4f209998f
NadinVi <vinokhodova.nadin@gmail.com>
2023-08-09 18:00:46 +0200

    add task 6

--- Lesson 7/task 6/app.js
+++ Lesson 7/task 6/app.js
@@ -1,2 +1,11 @@
 // Реалізуйте програму, яка відслідковуватиме зміну розміру (ширини і висоти) 
 // вікна браузера і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
+function displayWindow () {
+    let width = window.innerWidth;
+    let height= window.innerHeight;
+    let size = `Width: ${width}, Height: ${height}`;
+
+    document.getElementById("rezult").innerHTML = size;
+};
+
+window.addEventListener("resize", displayWindow);
